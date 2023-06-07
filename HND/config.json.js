08399a10058a80d2ae74c9d6b7cd587e8c5ba393
vj23456 <53201527+vj23456@users.noreply.github.com>
2023-06-07 17:22:27 +0800

    Update config.json.js

--- HND/config.json.js
+++ HND/config.json.js
@@ -4,11 +4,11 @@
 "home_url":"Module_merlinclash.asp",
 "name":"merlinclash",
 "title":"Merlin Clash",
-"version":"23.05.12",
+"version":"23.06.07",
 "patch_version":"000",
-"sc_version":"2023051201",
+"sc_version":"2023060701",
 "com_version":"2021060601",
 "game_version":"2020071901",
-"link":"https://t.me/merlinclashfile/459",
-"patch_link":"https://t.me/merlinclashfile/459"
+"link":"https://t.me/merlinclashfile/462",
+"patch_link":"https://t.me/merlinclashfile/462"
 }
