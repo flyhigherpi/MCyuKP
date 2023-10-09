39fa8eac1b16f303e8a76b15615dfd0b47b51c8b
vj23456 <53201527+vj23456@users.noreply.github.com>
2023-10-09 10:17:53 +0800

    Update config.json.js

--- QCA/config.json.js
+++ QCA/config.json.js
@@ -4,11 +4,11 @@
 "home_url":"Module_merlinclash.asp",
 "name":"merlinclash",
 "title":"Merlin Clash",
-"version":"23.08.16.2",
+"version":"23.10.09.2",
 "patch_version":"000",
-"sc_version":"2023081601",
+"sc_version":"2023100801",
 "com_version":"2021060601",
 "game_version":"2020071901",
-"link":"https://t.me/merlinclashfile/474",
-"patch_link":"https://t.me/merlinclashfile/474"
+"link":"https://t.me/merlinclashfile/480",
+"patch_link":"https://t.me/merlinclashfile/480"
 }
